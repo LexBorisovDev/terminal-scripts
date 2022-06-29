a9252ffa0fa646654ee64a819ca8c0c3318f3ca8
Lex Borisov <lborisov@plexxis.com>
2022-06-29 10:53:01 -0400

    terminal scripts

--- scripts/git-bash/profile-backup/aliases.sh
+++ scripts/git-bash/profile-backup/aliases.sh
@@ -5,6 +5,57 @@
 alias ls='ls -F --color=auto --show-control-chars'
 alias ll='ls -l'
 
+# ========= FUNCTIONS =========
+source /c/Users/lborisov/terminal/scripts/git-bash/shell/mkcd.sh
+source /c/Users/lborisov/terminal/scripts/git-bash/shell/show-items.sh
+source /c/Users/lborisov/terminal/scripts/git-bash/shell/show-history.sh
+source /c/Users/lborisov/terminal/scripts/git-bash/shell/delete-history.sh
+
+source /c/Users/lborisov/terminal/scripts/git-bash/shell/navigation/navigate.sh
+source /c/Users/lborisov/terminal/scripts/git-bash/shell/navigation/dev.sh
+source /c/Users/lborisov/terminal/scripts/git-bash/shell/navigation/practice.sh
+source /c/Users/lborisov/terminal/scripts/git-bash/shell/navigation/projects.sh
+source /c/Users/lborisov/terminal/scripts/git-bash/shell/navigation/go-back.sh
+source /c/Users/lborisov/terminal/scripts/git-bash/shell/navigation/go-forward.sh
+source /c/Users/lborisov/terminal/scripts/git-bash/shell/navigation/program-files.sh
+source /c/Users/lborisov/terminal/scripts/git-bash/shell/navigation/scripts.sh
+source /c/Users/lborisov/terminal/scripts/git-bash/shell/navigation/ps1.sh
+source /c/Users/lborisov/terminal/scripts/git-bash/shell/navigation/seneca.sh
+source /c/Users/lborisov/terminal/scripts/git-bash/shell/navigation/user.sh
+
+source /c/Users/lborisov/terminal/scripts/git-bash/dev/react/create-react-app.sh
+
+source /c/Users/lborisov/terminal/scripts/git-bash/apps/notepad.sh
+
+
+# ========= DEV =========
+alias p='practice'
+alias j='projects'
+alias cra='create-react-app'
+
+# ========= NAVIGATION =========
+alias gb='go-back'
+alias go='go-forward'
+alias gf='go-forward'
+alias pf='program-files'
+alias usr='user'
+alias sen='seneca'
+
+# ========= ALIASES =========
+alias edit-aliases='code ~/.bash_aliases'
+alias source-aliases='source ~/.bash_aliases'
+alias ea='edit-aliases'
+alias sa='source-aliases'
+
+# ========= SHELL =========
+alias c='clear'
+alias z='show-items'
+alias dh="delete-history"
+alias his="show-history"
+
+# ========= APPS =========
+alias np='notepad'
+
 case "$TERM" in
 xterm*)
 	# The following programs are known to require a Win32 Console
--- scripts/powershell/scripts/backup/git-bash/backup-gitbash.ps1
+++ scripts/powershell/scripts/backup/git-bash/backup-gitbash.ps1
@@ -1,8 +1,10 @@
 # profile
 $script:profileDirectory = "$env:ProgramFiles\Git\etc\profile.d"
+$script:bashrcFile = "$env:ProgramFiles\Git\etc\bash.bashrc"
 $script:profileBackup = "$env:UserProfile\terminal\scripts\git-bash\profile-backup"
 Remove-Item -Recurse -Path "$profileBackup\*"
 Copy-Item -Recurse -Path "$profileDirectory\*" -Destination "$profileBackup"
+Copy-Item -Path $bashrcFile  -Destination "$profileBackup"
 
 write-host "✅ backed up " -ForegroundColor DarkYellow -NoNewline
 write-host "Git-Bash" -ForegroundColor Blue -NoNewline
